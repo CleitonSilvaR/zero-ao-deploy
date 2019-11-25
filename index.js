74c0b0ad65472e8f7b22febcf60fb1b58fa5d4b4
Cleiton Silva <cleiton_.98@hotmail.com>
2019-11-25 15:44:07 -0300

    ajuste porta

--- index.js
+++ index.js
@@ -9,16 +9,16 @@ const index = ctx => {
 };
 
 const hello = ctx => {
-  const name = hello.parans.name || 'World';
+  const name = ctx.params.name || 'World';
   ctx.body = `Hello ${name}`;
 };
 
-router.get('/', index);
-router.get('/hello', hello);
 router.get('/hello/:name', hello);
+router.get('/hello', hello);
+router.get('/', ctx => (ctx.body = 'Node.JS do Zero ao Deploy'));
 
 app.use(router.routes());
 
-const port = 3000;
+const port = process.env.PORT;
 
 app.listen(port).on('listening', () => console.log(`Pooorta ${port}`));
